4437429d73e1acc91c1b2532b9b296c3c56120b6
Vijay Yennam <vjay11yenam@gmail.com>
2019-02-20 16:33:47 +0530

    Update app_config.js

--- configurations/app_config.js
+++ configurations/app_config.js
@@ -2,13 +2,17 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'local';
 process.env.PORT = process.env.PORT || 3001;
 
+/* Move to secrets*/
+const dbuser = `-server-admin`;
+const dbpassword = `admin1`;
+
 let config = {
     server: {
         host: process.env.HOST || '127.0.0.1',
         port: process.env.PORT
     },
     database: {
-        connect_uri: ''
+        connect_uri: `mongodb://${dbuser}:${dbpassword}@ds151232.mlab.com:51232/my-shelf`
     }
 };
 
